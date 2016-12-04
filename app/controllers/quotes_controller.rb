04e23e5b6c2f1ca80e4483b93de202f943d460ff
LesRedfield <les.redfield.dev@gmail.com>
2016-12-04 10:26:48 -0500

    Populate quotes controller and views

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,33 @@
 class QuotesController < ApplicationController
-  
+
+  def new
+
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.age && @quote.age < 18
+      flash.now[:errors] = ["#{@quote.name} is not eligible for a life insurance policy."]
+      render :new
+    elsif @quote.save
+      if params[:conditions]
+        params[:conditions].each do |condition_id|
+          @quote.add_condition(condition_id)
+        end
+      end
+
+      @price = @quote.generate_price
+      render :new
+    else
+      flash.now[:errors] = @quote.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:name, :city, :state_id, :age, :gender)
+  end
 end
